f2fbe7b41d83cb8775209028666863dbc7d42521
479626 <90899255+479626@users.noreply.github.com>
2022-09-14 14:40:10 +0100

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Car
+    {
+        private int direction;
+        private string manufacturer;
+        private float zeroToSixty;
+        private int speed;
+
+        public void InitialiseCar()
+        {
+            SteerCar(-1);
+            MoveCar(10);
+            SetManufacturer("Ariel");
+            Set0to60(2.3F);
+        }
+
+        public void SteerCar(int newDirection)
+        {
+            direction = newDirection;
+        }
+
+        public void MoveCar(int newSpeed)
+        {
+            speed = newSpeed;
+        }
+
+        public void Set0to60(float newZeroToSixty)
+        {
+            zeroToSixty = newZeroToSixty;
+        }
+
+        public void SetManufacturer(string newManufacturer)
+        {
+            manufacturer = newManufacturer;
+        }
+
+        public string GetManufacturer()
+        {
+            return manufacturer;
+        }
+
+        public string GetCarData()
+        {
+            String car =
+                "--------------------\n"
+                + "Your car\n"
+                + "\nManufacturer: " + manufacturer
+                + "\nSpeed: " + speed.ToString()
+                + "\nZero to Sixty: " + zeroToSixty.ToString()
+                + "\nDirection: " + direction.ToString()
+                + "\n--------------------";
+
+            return car;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp1;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.InitialiseCar();
+        Console.WriteLine(car.GetCarData());
+    }
+}
